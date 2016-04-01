2c79cfb69146dccf2851664b2428ba503fe63ee7
J.T. Yim <JT@JTYim.com>
2016-04-01 16:09:50 -0400

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  host: process.env.CLEARDB_DATABASE_URL || '',
-  user: process.env.CLEARDB_DATABASE_USER || '',
-  password: process.env.CLEARDB_DATABASE_PASS || '',
-  database: process.env.CLEARDB_DATABASE || ''
-};
